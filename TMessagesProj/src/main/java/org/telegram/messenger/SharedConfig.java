cc27c5272e69d6f415cb4d620e45df6e85e9eaec
23rd <23rd@vivaldi.net>
2018-10-27 16:41:40 +0300

    Added item in dialog menu to hide title and avatar in dialog.

--- TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
@@ -230,6 +230,8 @@ public class SharedConfig {
     public static boolean isFloatingDebugActive;
     public static LiteMode liteMode;
 
+    public static boolean hideTitleDialog = false;
+
     private static final int[] LOW_SOC = {
             -1775228513, // EXYNOS 850
             802464304,  // EXYNOS 7872
@@ -526,6 +528,8 @@ public class SharedConfig {
 
             preferences = ApplicationLoader.applicationContext.getSharedPreferences("Notifications", Activity.MODE_PRIVATE);
             showNotificationsForAllAccounts = preferences.getBoolean("AllAccounts", true);
+
+            hideTitleDialog = preferences.getBoolean("hideTitle", false);
 
             configLoaded = true;
 
