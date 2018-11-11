8601f39564d42fe0f794f20985bb6d2c0d43e693
23rd <23rd@vivaldi.net>
2018-11-11 15:38:29 +0300

    Owner only. Added auto inserting dot to the end of message.
     - Triple space to avoid adding a dot.
     - Added more last symbols to avoid adding a dot.
     - Replace triple dot to … symbol.
     - Don't add dot if message ends with Mono.
     - Don't add dot if message ends with +.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
@@ -571,6 +571,8 @@ public class ChatActivityEnterView extends BlurredFrameLayout implements Notific
     private boolean allowStickers;
     private boolean allowGifs;
 
+    private boolean skipDotAtEnd = false;
+
     private int lastSizeChangeValue1;
     private boolean lastSizeChangeValue2;
 
@@ -3670,6 +3672,11 @@ public class ChatActivityEnterView extends BlurredFrameLayout implements Notific
                     }
                     sendMessageInternal(false, 0, true);
                 });
+                sendWithoutSoundButton.setLongClickable(true);
+                sendWithoutSoundButton.setOnLongClickListener(v -> {
+                    skipDotAtEnd = true;
+                    return sendWithoutSoundButton.performClick();
+                });
                 sendPopupLayout.addView(sendWithoutSoundButton, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 48));
             }
             sendPopupLayout.setupRadialSelectors(getThemedColor(Theme.key_dialogButtonSelector));
@@ -5761,7 +5768,45 @@ public class ChatActivityEnterView extends BlurredFrameLayout implements Notific
         setEditingMessageObject(null, false);
     }
 
+    private String processDottedString(
+            String textMessageString,
+            ArrayList<TLRPC.MessageEntity> entities) {
+        if (skipDotAtEnd) {
+            skipDotAtEnd = false;
+            return textMessageString;
+        }
+        boolean endsWithRichText = false;
+        if ((entities != null) && (entities.size() > 0)) {
+            TLRPC.MessageEntity last = entities.get(entities.size() - 1);
+            if (last instanceof TLRPC.TL_messageEntityCode
+                || last instanceof TLRPC.TL_messageEntityUrl
+                || last instanceof TLRPC.TL_messageEntityEmail) {
+                endsWithRichText = last.offset + last.length == textMessageString.length();
+            }
+        }
+
+        if (textMessageString.endsWith("...")) {
+            textMessageString = textMessageString.replace("...", "…");
+        }
+        if (!textMessageString.endsWith(".")
+            && !textMessageString.endsWith("!")
+            && !textMessageString.endsWith("…")
+            && !textMessageString.endsWith("?")
+            && !textMessageString.endsWith("+")
+            && !textMessageString.endsWith("=)")
+            && !textMessageString.endsWith("=(")
+            && !textMessageString.endsWith(".)")
+            && !endsWithRichText
+            && !textMessageString.startsWith("/")) {
+            textMessageString += ".";
+        }
+        return textMessageString;
+    }
+
     public boolean processSendingText(CharSequence text, boolean notify, int scheduleDate) {
+        final boolean isOwner = UserConfig.getInstance(currentAccount).clientUserId == 
+            org.telegram.messenger.BuildVars.USER_ID_OWNER;
+
         int[] emojiOnly = new int[1];
         Emoji.parseEmojis(text, emojiOnly);
         boolean hasOnlyEmoji = emojiOnly[0] > 0;
@@ -5836,8 +5881,11 @@ public class ChatActivityEnterView extends BlurredFrameLayout implements Notific
                 boolean updateStickersOrder = false;
                 updateStickersOrder = SendMessagesHelper.checkUpdateStickersOrder(text);
 
+                final String textMessageString = isOwner
+                    ? processDottedString(message[0].toString(), entities)
+                    : message[0].toString();
 
-                SendMessagesHelper.SendMessageParams params = SendMessagesHelper.SendMessageParams.of(message[0].toString(), dialog_id, replyingMessageObject, getThreadMessage(), messageWebPage, messageWebPageSearch, entities, null, null, notify, scheduleDate, sendAnimationData, updateStickersOrder);
+                SendMessagesHelper.SendMessageParams params = SendMessagesHelper.SendMessageParams.of(textMessageString, dialog_id, replyingMessageObject, getThreadMessage(), messageWebPage, messageWebPageSearch, entities, null, null, notify, scheduleDate, sendAnimationData, updateStickersOrder);
                 applyStoryToSendMessageParams(params);
                 SendMessagesHelper.getInstance(currentAccount).sendMessage(params);
                 start = end + 1;
