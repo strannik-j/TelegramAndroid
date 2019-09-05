a085566e36678479c7a88da0901ffcb46affe51d
23rd <23rd@vivaldi.net>
2019-09-05 19:31:52 +0300

    Added setting to hide or show personal information.

--- TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
@@ -1477,4 +1477,17 @@ public class SharedConfig {
     public static boolean deviceIsAverage() {
         return getDevicePerformanceClass() <= PERFORMANCE_CLASS_AVERAGE;
     }
+
+    public static boolean hideSensitiveData() {
+        if (isUserOwner()) {
+            return true;
+        }
+        return MessagesController.getGlobalMainSettings().getBoolean("hideSensitiveData", false);
+    }
+
+    public static boolean isUserOwner() {
+        return org.telegram.messenger.UserConfig.getInstance(
+                org.telegram.messenger.UserConfig.selectedAccount).clientUserId ==
+                    org.telegram.messenger.BuildVars.USER_ID_OWNER;
+    }
 }
