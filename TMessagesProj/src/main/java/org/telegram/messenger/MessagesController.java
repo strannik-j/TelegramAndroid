64f5339c636f39ffd468a4d687cacc56ea087fbd
23rd <23rd@vivaldi.net>
2019-09-05 20:23:29 +0300

    Added setting to turn off sync pins to make unlimited number of pins.

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -12359,11 +12359,13 @@ public class MessagesController extends BaseController implements NotificationCe
                     newTaskId = taskId;
                 }
 
-                getConnectionsManager().sendRequest(req, (response, error) -> {
-                    if (newTaskId != 0) {
-                        getMessagesStorage().removePendingTask(newTaskId);
-                    }
-                });
+                if (mainPreferences.getBoolean("syncPins", true)) {
+                    getConnectionsManager().sendRequest(req, (response, error) -> {
+                        if (newTaskId != 0) {
+                            getMessagesStorage().removePendingTask(newTaskId);
+                        }
+                    });
+                }
             }
         }
         getMessagesStorage().setDialogPinned(dialogId, dialog.pinnedNum);
@@ -12371,6 +12373,9 @@ public class MessagesController extends BaseController implements NotificationCe
     }
 
     public void loadPinnedDialogs(final int folderId, long newDialogId, ArrayList<Long> order) {
+        if (!mainPreferences.getBoolean("syncPins", true)) {
+            return;
+        }
         if (loadingPinnedDialogs.indexOfKey(folderId) >= 0 || getUserConfig().isPinnedDialogsLoaded(folderId)) {
             return;
         }
