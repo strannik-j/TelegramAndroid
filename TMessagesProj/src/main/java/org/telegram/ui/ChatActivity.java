4e74e607fb95dc4129fa7b98f84872c20681a012
23rd <23rd@vivaldi.net>
2019-10-08 12:24:21 +0300

    Added ability to mention users by name.
     - Fixed #14.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -7173,7 +7173,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 if (searchingForUser && searchContainer.getVisibility() == View.VISIBLE) {
                     searchUserMessages(user, null);
                 } else {
-                    if (UserObject.getPublicUsername(user) != null) {
+                    SharedPreferences preferences = MessagesController.getGlobalMainSettings();
+                    final boolean mentionByName = !user.bot && preferences.getBoolean("mentionByName", false);
+                    if (UserObject.getPublicUsername(user) != null && !mentionByName) {
                         chatActivityEnterView.replaceWithText(start, len, "@" + UserObject.getPublicUsername(user) + " ", false);
                     } else {
                         String name = UserObject.getFirstName(user, false);
