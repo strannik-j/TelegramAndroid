f38f950c1c7564dee82d9e442af4d6c49b304f1c
23rd <23rd@vivaldi.net>
2020-07-21 00:10:49 +0300

    Added option to disable thumbs in dialog list.

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -1240,6 +1240,10 @@ public class DialogCell extends BaseCell {
                             }
                         }
                     } else {
+                        final boolean disableThumbs = MessagesController
+                            .getGlobalMainSettings()
+                            .getBoolean("disableThumbsInDialogList", false);
+
                         String restrictionReason = MessagesController.getRestrictionReason(message.messageOwner.restriction_reason);
                         TLRPC.User fromUser = null;
                         TLRPC.Chat fromChat = null;
@@ -1364,6 +1368,7 @@ public class DialogCell extends BaseCell {
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     if (thumbInsertIndex >= builder.length()) {
                                         builder.append(" ");
@@ -1371,6 +1376,7 @@ public class DialogCell extends BaseCell {
                                     } else {
                                         builder.insert(thumbInsertIndex, " ");
                                         builder.setSpan(new FixedWidthSpan(AndroidUtilities.dp(thumbsCount * (thumbSize + 2) - 2 + 5)), thumbInsertIndex, thumbInsertIndex + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                    }
                                     }
                                 }
                             } else {
@@ -1489,6 +1495,7 @@ public class DialogCell extends BaseCell {
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     builder.insert(0, " ");
                                     builder.setSpan(new FixedWidthSpan(AndroidUtilities.dp((thumbSize + 2) * thumbsCount - 2 + 5)), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -1498,6 +1505,7 @@ public class DialogCell extends BaseCell {
                                         if (s != null) {
                                             messageString = s;
                                         }
+                                    }
                                     }
                                 }
                             }
@@ -3623,7 +3631,10 @@ public class DialogCell extends BaseCell {
                 Theme.dialogs_pinnedDrawable.draw(canvas);
             }
 
-            if (thumbsCount > 0 && updateHelper.typingProgres != 1f) {
+            if (thumbsCount > 0 && updateHelper.typingProgres != 1f
+                && !MessagesController
+                    .getGlobalMainSettings()
+                    .getBoolean("disableThumbsInDialogList", false)) {
                 float alpha = 1f;
                 if (updateHelper.typingProgres > 0) {
                     alpha = (1f - updateHelper.typingProgres);
@@ -4486,6 +4497,12 @@ public class DialogCell extends BaseCell {
 
     public void updateMessageThumbs() {
         if (message == null) {
+            return;
+        }
+        final boolean disableThumbs = MessagesController
+            .getGlobalMainSettings()
+            .getBoolean("disableThumbsInDialogList", false);
+        if (disableThumbs) {
             return;
         }
         String restrictionReason = MessagesController.getRestrictionReason(message.messageOwner.restriction_reason);
