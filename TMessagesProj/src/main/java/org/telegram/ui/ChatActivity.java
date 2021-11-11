ec52a17460d2b7691d15958f5a7f6ea65f98b60b
23rd <23rd@vivaldi.net>
2021-11-11 08:29:53 +0300

    Added ability to delete all your messages in group.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -1427,6 +1427,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private final static int video_call = 33;
     private final static int hideTitle = 34;
     private final static int goToFirstMessage = 35;
+    private final static int deleteAllYourMessages = 36;
 
     private final static int attach_photo = 0;
     private final static int attach_gallery = 1;
@@ -2629,6 +2630,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     // This is timestamp of launch date of the Telegram.
                     // August 2013.
                     jumpToDate(1375350800);
+                } else if (id == deleteAllYourMessages) {
+                    org.telegram.messenger.forkgram.ForkDialogs.CreateDeleteAllYourMessagesAlert(
+                        currentAccount,
+                        dialog_id,
+                        getParentActivity());
                 }
             }
         });
@@ -3005,6 +3011,13 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 headerItem.addSubItem(bot_settings, R.drawable.menu_settings, LocaleController.getString("BotSettings", R.string.BotSettings), themeDelegate);
                 headerItem.addSubItem(bot_help, R.drawable.menu_help, LocaleController.getString("BotHelp", R.string.BotHelp), themeDelegate);
                 updateBotButtons();
+            }
+            if (ChatObject.isMegagroup(currentChat) || currentChat != null && !ChatObject.isChannel(currentChat)) {
+                headerItem.addSubItem(
+                    deleteAllYourMessages,
+                    R.drawable.msg_delete,
+                    LocaleController.getString("DeleteAllYourMessages", R.string.DeleteAllYourMessages),
+                    themeDelegate);
             }
 
             String hideTitleString = "";
