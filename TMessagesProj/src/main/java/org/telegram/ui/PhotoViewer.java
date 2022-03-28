051b508a9db27018f6f1b0d9bbb5bad451cf17e8
thermatk <thermatk@thermatk.com>
2022-03-28 23:47:04 +0200

    [TF][KILL] Google Vision

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -130,9 +130,6 @@ import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.analytics.AnalyticsListener;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.FaceDetector;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.AnimationNotificationsLocker;
@@ -10265,6 +10262,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
             return;
         }
         Utilities.globalQueue.postRunnable(() -> {
+        	/*
             FaceDetector faceDetector = null;
             try {
                 faceDetector = new FaceDetector.Builder(ApplicationLoader.applicationContext)
@@ -10285,7 +10283,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                 } else {
                     if (BuildVars.LOGS_ENABLED) {
                         FileLog.e("face detection is not operational");
-                    }
+                    }*/
                     AndroidUtilities.runOnUIThread(() -> {
                         bitmap.release();
                         String imageKey = centerImage.getImageKey();
@@ -10293,7 +10291,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                             currentImageHasFace = 2;
                             currentImageFaceKey = key;
                         }
-                    });
+                    });/*
                 }
             } catch (Exception e) {
                 FileLog.e(e);
@@ -10301,7 +10299,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                 if (faceDetector != null) {
                     faceDetector.release();
                 }
-            }
+            }*/
         });
     }
 
